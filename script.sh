67d85b081412b830f5902edd11f6f04237899872
Ubuntu <mahipal2@LoadTesting3.fozhy5rmm4ge5mi4tagj2dgswd.bx.internal.cloudapp.net>
2020-03-30 07:59:35 +0000

    Store Logs

--- script.sh
+++ script.sh
@@ -2,10 +2,16 @@ i=1
 while IFS= read -r line; do
     echo "Text read from file: $line"
     echo "https://classtranscribe.ncsa.illinois.edu$line"
-    curl -s --limit-rate 660k --output /dev/null "https://classtranscribe.ncsa.illinois.edu$line" &
+    logfile=`echo $line|cut -d'-' -f 2`
+    logfile="../logs/$logfile.txt"
+    echo $logfile
+    curl --limit-rate 660k --output /dev/null "https://classtranscribe.ncsa.illinois.edu$line" -w "%{time_total},%{size_download},%{speed_download}\n" &> $logfile &
     echo "Sleeping"
     sleep 3
     echo "Slept $i"
     i=$((i + 1))
-    #break
+    # if [ $i -eq 3 ]
+    # then
+    #     break
+    # fi
 done < files.txt
